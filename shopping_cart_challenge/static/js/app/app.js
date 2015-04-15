49ba7b80c84ada939f9d197303d0502be528cef9
paulkore <paulkore@outlook.com>
2015-04-15 15:27:31 -0400

    Refactoring to improve separation of concerns
    Refactor implementation to de-couple the following concepts, and make
    the code more modular:
    
    - load product catalogue data
    - load existing order data
    - prepare form for new order
    - prepare form for existing order (edit, review, confirmed modes)

--- shopping_cart_challenge/static/js/app/app.js
+++ shopping_cart_challenge/static/js/app/app.js
@@ -4,45 +4,44 @@ appModule.controller("OrderController", function($scope, $location, $http) {
 
         $scope.formTitle = 'New order';
         $scope.orderId = null;
-        $scope.orderStatus = null;
-        $scope.products = null;
+        $scope.orderStatus = 'EDIT';
+        $scope.orderProducts = null;
+        $scope.catalogueProducts = null;
+        $scope.formProducts = null;
         $scope.ajaxError = null;
 
         // decide whether it's a new or an existing order based on the URL
-        // (feels wrong, but it'll do for now)
+        // (this feels wrong, but it'll do for now)
         $scope.url = $location.absUrl();
         console.log('url = ' + $scope.url);
         if (!$scope.url.contains('orders/new')) {
+            $scope.formTitle = 'Existing order'
+
             // TODO: improve this ID extraction code, as it is unreliable (it grabs the right-most numeric value)
             var numericValues = $scope.url.match( /\d+/g);
             $scope.orderId = Number(numericValues.last());
         }
 
 
+        $scope.loadCatalogueProducts = function() {
+            console.log('loading list of all products');
 
-        $scope.loadEmptyOrder = function() {
-            console.log('loading data for empty order');
+            $scope.catalogueProducts = null;
+            $scope.ajaxError = null;
 
             var promise = $http.get("/api/products");
             promise.success(function(data) {
-                $scope.products = [];
+                $scope.catalogueProducts = [];
                 for (var i=0; i<data.length; i++) {
                     var product_data = data[i];
                     var product = {};
                     product.id = product_data.id;
                     product.name = product_data.name;
                     product.price = product_data.price;
-                    product.check = false;
-                    product.quantity = null;
-
-                    $scope.products.push(product)
+                    $scope.catalogueProducts.push(product)
                 }
-
-                $scope.formTitle = 'New order';
-                $scope.ajaxError = null;
             });
             promise.error(function(data) {
-                $scope.products = null;
                 $scope.ajaxError = 'Could not load data (AJAX call failed)';
             });
 
@@ -52,60 +51,99 @@ appModule.controller("OrderController", function($scope, $location, $http) {
         $scope.loadExistingOrder = function() {
             console.log('loading data for existing order: ' + $scope.orderId);
 
-            console.log('first, load empty order template (all products)');
-            $scope.loadEmptyOrder().then(function() {
-                if ($scope.ajaxError) {
-                    return;
+            $scope.orderProducts = null;
+            $scope.ajaxError = null;
+
+            var promise = $http.get("/api/orders/" + $scope.orderId);
+            promise.success(function(data) {
+                $scope.orderStatus = data.order_status;
+                $scope.orderProducts = data.products;
+            });
+            promise.error(function(data) {
+                $scope.ajaxError = 'Could not load data (AJAX call failed)';
+            });
+
+            return promise;
+        };
+
+
+        $scope.prepareNewOrderForm = function() {
+            console.log('preparing form for new order');
+
+            $scope.loadCatalogueProducts().then(function() {
+                $scope.formProducts = [];
+
+                for (var i=0; i<$scope.catalogueProducts.length; i++) {
+                    var formProduct = {};
+                    formProduct.product = $scope.catalogueProducts[i];
+                    formProduct.check = false;
+                    formProduct.quantity = null;
+                    $scope.formProducts.push(formProduct);
+                }
+            });
+        };
+
+        $scope.prepareExistingOrderForm = function() {
+            console.log('preparing form for existing order');
+
+            $scope.loadExistingOrder().then(function(){
+                $scope.formProducts = [];
+
+                if ($scope.orderStatus == 'CONFIRMED' || $scope.orderStatus == 'REVIEW') {
+                    console.log('preparing form for order in review, or confirmed order');
+
+                    for (var i=0; i<$scope.orderProducts.length; i++) {
+                        var orderProduct = $scope.orderProducts[i];
+
+                        var formProduct = {};
+                        formProduct.product = orderProduct.product;
+                        formProduct.check = true;
+                        formProduct.quantity = orderProduct.quantity;
+                        $scope.formProducts.push(formProduct);
+                    }
+                }
+                else if ($scope.orderStatus == 'EDIT') {
+                    console.log('preparing form for order being edited - need to load the catalogue');
+
+                    $scope.loadCatalogueProducts().then(function() {
+
+                        // 'new order' form used as a base for 'edit order' form
+                        for (var i=0; i<$scope.catalogueProducts.length; i++) {
+                            var formProduct = {};
+                            formProduct.product = $scope.catalogueProducts[i];
+                            formProduct.check = false;
+                            formProduct.quantity = null;
+                            $scope.formProducts.push(formProduct);
+                        }
+
+                        // populate the blank form with data from the existing order
+                        for (var j=0; j<$scope.orderProducts.length; j++) {
+                            var orderProduct = $scope.orderProducts[j];
+                            var formProd = $scope.findFormProduct(orderProduct.product.id);
+
+                            formProd.check = true;
+                            formProd.quantity = orderProduct.quantity;
+                        }
+                    });
+                }
+                else {
+                    console.error('Unhandled order status: ' + $scope.orderStatus)
                 }
 
-                var promise = $http.get("/api/orders/" + $scope.orderId);
-                promise.success(function(data) {
-                    $scope.orderStatus = data.order_status;
-                    var product_quantity_list = data.products;
-                    for (var i=0; i<product_quantity_list.length; i++) {
-                        var product_quantity_data = product_quantity_list[i];
-                        var product_data = product_quantity_data.product;
-
-                        var product = $scope.findProduct(product_data.id);
-                        if (product) {
-                            product.quantity = product_quantity_data.quantity;
-                            product.check = product_quantity_data.quantity > 0;
-                        }
-                    }
-
-                    if ($scope.orderStatus == 'EDIT') {
-                        $scope.formTitle = 'Edit order';
-                    }
-                    else if ($scope.orderStatus == 'REVIEW') {
-                        $scope.formTitle = 'Review order';
-                    }
-                    else {
-                        $scope.formTitle = 'Foo'
-                    }
-
-                    $scope.ajaxError = null;
-                });
-                promise.error(function(data) {
-                    $scope.products = null;
-                    $scope.ajaxError = 'Could not load data (AJAX call failed)';
-                });
             });
 
         };
 
-        $scope.findProduct = function(product_id) {
-            if (!$scope.products) {
-                return;
-            }
-
+        $scope.findFormProduct = function(productId) {
             // TODO: sequential search... not ideal, but it will do for now.
-            for (var i=0; i<$scope.products.length; i++) {
-                var product = $scope.products[i];
-                if (product.id === product_id) {
-                    return product;
+            for (var i=0; i<$scope.formProducts.length; i++) {
+                var formProduct = $scope.formProducts[i];
+                if (formProduct.id === productId) {
+                    return formProduct;
                 }
             }
 
+            console.error('form product not found for id: ' + productId);
             return null;
         };
 
@@ -137,10 +175,10 @@ appModule.controller("OrderController", function($scope, $location, $http) {
         if ($scope.autoLoad) {
             console.log('auto load (AJAX)');
             if ($scope.orderId) {
-                $scope.loadExistingOrder();
+                $scope.prepareExistingOrderForm();
             }
             else {
-                $scope.loadEmptyOrder();
+                $scope.prepareNewOrderForm();
             }
         }
 
